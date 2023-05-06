b19e213394eea07bcd31141846d2ba13730508fe
Sagar-Naik-B <sagarnaikbukkya@gmail.com>
2023-05-06 11:35:14 +0530

    refact(*): commet added

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
+
+// entry here
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
